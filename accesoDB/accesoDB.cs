a697760d682df64de7c11499b567bb811a32c8b0
Juan Sebastián Figueroa <juan.psi.figueroa@gmail.com>
2024-07-29 19:17:46 -0300

    Creacion de los metodos de acceso a DB

--- accesoDB/accesoDB.cs
+++ accesoDB/accesoDB.cs
@@ -3,10 +3,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace accesoDB
 {
-    internal class accesoDB
+    public class accesoDB
     {
+        //Atributos. Metemos los dos using para que los tome
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        //Constructor
+        public accesoDB()
+        {
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["cadenaConexion"]);
+            //Otra forma de hacer la conexion
+            //conexion = new SqlConnection("server=.\\SQLEXPRESS; database=POKEDEX_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        //Metodos
+        public SqlDataReader Lector //para que solo devuelva el lector
+        {
+            get { return lector; }
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        //en esta app no manejamos stored procedure, si la DB tuviese usar el siguiente metodo
+        //public void setearProcedimiento(string sp)
+        //{
+        //    comando.CommandType = System.Data.CommandType.StoredProcedure;
+        //    comando.CommandText = sp;
+        //}
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        //usada para modificar registros en DB
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        //Para devolver un indice, o ID de un registro de la DB
+        public int ejecutarAccionScalar()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                //comando.ExecuteScalar devuelve el valor de la primera columna y primera fila de la consulta
+                return int.Parse(comando.ExecuteScalar().ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //Permite pasar valores por parametros a las consultas
+        public void setearParametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void cerrarConexion()
+        {
+            if(lector != null)
+                lector.Close();
+            conexion.Close();
+        }
     }
 }
